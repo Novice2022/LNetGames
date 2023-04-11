4b9124d17a46d19b7b7c91b24294822ee5c9465d
Novice2022 <96382025+Novice2022@users.noreply.github.com>
2023-04-12 01:52:54 +0300

    Update
    * added new file to install and upgrade and notice (rename to cman.py if it has other name)
    * rewritten install, get_installed_files methods and other little things

--- Cman.py
+++ Cman.py
@@ -1,4 +1,4 @@
-#  version: 2.2. Release: 09.04.2023
+#  version: 2.3. Release: 12.04.2023
 
 import platform, re
 from os import system
@@ -6,6 +6,8 @@ from time import sleep
 from subprocess import check_output, CalledProcessError
 
 _OS = platform.system()
+
+del platform
 
 def clear_console() -> int: ...
 def get_current_IP() -> str: ...
@@ -18,17 +20,33 @@ if _OS == 'Windows':
     def get_ethernet_info() -> str:
         return console_output('ipconfig')
     def get_installed_files() -> list[str]:
-        return list(set(re.findall(r'\w*\.py', console_output('dir'))).intersection(set(['client.py', 'cman.py', 'games.py'])))
+        files = set(re.findall(r'\w*\.py', console_output('dir')))
+        if not 'cman.py' in files:
+            print()
+            print('\t/    please rename this file to \"cman.py\" make it avaliable to update this file automatically    \ ')
+            print('\t\   (some systems don\'t give permition for programs to rename files. Thanks for understanding)   /\n\n')
+            input('Enter to continue...')
+            print()
+        files = list(files.intersection(set(['client.py', 'cman.py', 'games.py', 'converter.py'])))
+        return files
 elif _OS == 'Linux':
     def clear_console() -> int: system('clear')
     def console_output(command: str) -> str: return check_output(command, shell=True, encoding='utf-8')
     def get_ethernet_info() -> str:
         return console_output('ifconfig')
     def get_installed_files() -> list[str]:
-        return list(set([_file for _file in console_output('dir').replace('\n', '  ').replace('\t', '  ').split('  ')]).intersection(set(['client.py', 'cman.py', 'games.py'])))
+        files = set(re.findall(r'\w*\.py', console_output('dir')))
+        if not 'cman.py' in files:
+            print()
+            print('\t/    please rename this file to \"cman.py\" make it avaliable to update this file automatically    \ ')
+            print('\t\   (some systems don\'t give permition for programs to rename files. Thanks for understanding)   /\n\n')
+            input('Enter to continue...')
+            print()
+        files = list(files.intersection(set(['client.py', 'cman.py', 'games.py', 'converter.py'])))
+        return files
 
 __author__ = 'Igor Zabrodin (@YandexFindMe / https://t.me/YandexFindMe)'
-__version__ = '2.2'
+__version__ = '2.3'
 __program_status__ = 1
 
 
@@ -45,14 +63,15 @@ except ModuleNotFoundError:
             try:
                 console_output('pip install requests')
             except CalledProcessError:
-                print('\tYou can install this package by youself using \'pip\'.')
-                print('\tThis manager can\'t do this because you didn\'t put \'pip\' manager to os environment path or you use other python version which \'pip\' doesn\'t in path or overloaded.')
+                print('\tYou can install this package by yourself using \'pip\'.')
+                print('\tThis manager can\'t do this because you didn\'t put \'pip\' to os environment path or you use other python version which \'pip\' doesn\'t in path or overloaded.')
                 print('\tYou can install python again with \'add to path\' configuration in setuper window.')
                 print('\tSee info about \'pip\' into your browser or write (@YandexFindMe / https://t.me/YandexFindMe) if you didn\'t understand.')
+                print('\tPlease do second one if you really have no idea.')
             try:
                 import requests
             except ModuleNotFoundError:
-                print('\tProblem in your \'pip\' configurations.\n\tIt is being fixed.')
+                print('\tProblem in your \'pip\' configurations.\n\tSolving automatically will be later.')
                 __program_status__ = -2
                 break
         elif command == 'n':
@@ -61,50 +80,51 @@ except ModuleNotFoundError:
 if __program_status__ == 1:
     clear_console()
 
-#  ----------------------------------------------------------------------  end main logic  ----------------------------------------------------------------------
+#  ---------------------------------------------------------------------  start main logic  ---------------------------------------------------------------------
 
 
 class Сlient_manager():
-    """ Represents installing, uninstalling, configurating & editing files of Your client application """
+    """ Represents installing and upgrading actual files """
 
     def __init__(self) -> None:
-        __description = f'Client manager application: install, upgrade & add LNetGames\'s games\n{__version__}\n\nPlease, DON\'T REMOVE OR RENAME files which this manager handle!'
+        __description = f'Client manager application: install, upgrade, work & enjoy with LNetGames\'s applications\nVersion: {__version__}\n\nDON\'T REMOVE or RENAME files which this manager handle (rename only this file to \"cman.py\" after installing)!'
         print(__description)
+        self.__support()
         command = input('\n> ').lower()
-        while not (command in ['get client', 'get games', 'help', '-help', '--help', '?', 'exit', 'upgrade', 'clear']):
+        while not (command in ['get client', 'get games', 'get converter', 'help', '-help', '--help', '?', 'exit', 'upgrade', 'clear']):
             print('No such command for client manager :(')
             command = input('\n> ')
         
         while command.lower() != 'exit':
             if command in ['help', '-help', '--help', '?']:
-                self._support()
-            elif command == 'get client':
-                self._install('client')
-            elif command == 'get games':
-                self._install('games')
+                self.__support()
+            print()
+            if command in ['get client', 'get games', 'get converter']:
+                self.__install(command.split()[1])
             elif command == 'upgrade':
-                self._upgrade()
+                self.__upgrade()
             elif command == 'clear':
                 clear_console()
                 print(__description)
             command = input('\n> ')
 
-    def _support(self) -> None:
-        print('\n\n\tYou may enter this commands with different cases\n\t------------------------------------------------')
+    def __support(self) -> None:
+        print('\n\n\t   You can enter this commands with different cases  \n\t-----------------------------------------------------')
         print('\tget client                   install client')
         print('\tget games                    install games')
+        print('\tget converter                install converter')
         print('\tupgrade                      upgrade all scripts')
         print('\thelp / -help / --help / ?    get this window')
         print('\texit                         exit from client manager')
         print('\tclear                        clear current window\n')
 
-    def _handle_connection_status(self) -> str:
+    def __handle_connection_status(self) -> str:
         def __connection_status() -> bool:
             ethernet_info = get_ethernet_info()
             return True if any(['IPv4' in ethernet_info if _OS == 'Windows' else 'inet 192.168']) else False
 
         if not __connection_status() or __connection_status() == None:
-            move = input('No interner connection.\n\t* "continue": wait internet connection\n\t* "break": turn to client-manager\n\t> ')
+            move = input('No internet connection.\n\t* "continue": wait internet connection\n\t* "break": turn to client-manager\n\t> ')
             while not move.lower() in ['continue', 'break']:
                 move = input('\n\t* "continue": wait internet connection\n\t* "break": turn to client-manager\n\t> ')
             if move == 'break': return 'break'
@@ -116,45 +136,44 @@ class Сlient_manager():
             timer += 0.5
             if timer % 10 == 0: print('\twaiting internet connection')
 
-    def _install(self, file_name: str) -> bool:
-        if self._handle_connection_status() == 'break':
+    def __install(self, file_name: str) -> None:
+        if self.__handle_connection_status() == 'break':
             return 'client break'
-        url = 'https://raw.githubusercontent.com/Novice2022/LNetGames/main/Games.py' if file_name == 'games' else 'https://raw.githubusercontent.com/Novice2022/LNetGames/main/Client.py'
+        url = f'https://raw.githubusercontent.com/Novice2022/LNetGames/main/{file_name[0].upper() + file_name[1:]}.py'
         response = requests.get(url).text
-        compare_versions_flag = self.__compare_versions(f'{file_name}.py', response.split('\n')[0][3:-1])
-        if compare_versions_flag == 'not found':
+        if self.__compare_versions(f'{file_name}.py', response.split('\n')[0][3:-1]) == 'not found':
             with open(f'{file_name}.py', 'w', encoding='utf-8') as f:
                 f.write(response.replace('\n', ''))
-                if compare_versions_flag == 'not found':
-                    print(f'\tInst: {file_name}.py ')
-                    return True
+                print(f'\tInst: {file_name}.py ')
+                return None
         print(f'\t{file_name}.py actually inastalled. Try \"upgrade\"')
-        return True
 
-    def _upgrade(self) -> str or bool:
-        if self._handle_connection_status() == 'break':
+    def __upgrade(self) -> str or None:
+        if self.__handle_connection_status() == 'break':
             return 'client break'
-        for _file in get_installed_files():
-            response = requests.get(f'https://raw.githubusercontent.com/Novice2022/LNetGames/main/{_file[0].upper() + _file[1:]}').text
-            versions = self.__compare_versions(_file, response)
+        for file_name in get_installed_files():
+            response = requests.get(f'https://raw.githubusercontent.com/Novice2022/LNetGames/main/{file_name[0].upper() + file_name[1:]}').text
+            versions = self.__compare_versions(file_name, response)
             if 'update' in versions:
                 versions = versions.replace('update ', '')
-                with open(f'{_file}', 'w', encoding='utf-8') as f:
+                with open(f'{file_name}', 'w', encoding='utf-8') as f:
                     f.write(response.replace('\n', ''))
-                    print(f'\tUpg: {_file} {versions}')
+                    print(f'\tUpg: {file_name} {versions}')
             else:
-                print(f'\t{_file} has latest version')
+                print(f'\t{file_name} has latest version')
 
     @staticmethod
     def __compare_versions(exists_file_path: str, request_version: str) -> str:
         try:
             current_version = open(exists_file_path, 'r', encoding='utf-8').readline()[3:-1]
             request_version = request_version.split('\n')[0][3:-1]
-            if current_version != request_version: return f'update {current_version} => {request_version}'
+            if current_version != request_version: return f'update \t\t[{current_version}  =>  {request_version}]'
             return 'OK'
         except FileNotFoundError:
             return 'not found'
 
+
+#  ----------------------------------------------------------------------  finish main logic  ----------------------------------------------------------------------
 
 if __name__ == "__main__":
     if __program_status__ == 1:
@@ -162,6 +181,8 @@ if __name__ == "__main__":
         try:
             Сlient_manager()
         except Exception:
-            print('\n\tYou probably aren\'t connected to the internet.\n\tRefresh internet connection and retry.\n\tThis problem for Linux is being solved.')
+            print('\n\tYou probably aren\'t connected to the internet.\n\tRefresh internet connection and retry.')
             __program_status__ = -1
     print(f'\nProgram status: {__program_status__}')
+
+#  ----------------------------------------------------------------------  finish main logic  ----------------------------------------------------------------------
